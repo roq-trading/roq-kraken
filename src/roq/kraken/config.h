36918d17e1726c7f26fd65d3d338f09e0b94b694
thraneh <thraneh@gmail.com>
2020-07-08 07:30:51 +0200

    use absl::flat_hash_{map,set} wherever possible

--- src/roq/kraken/config.h
+++ src/roq/kraken/config.h
@@ -7,9 +7,9 @@
 #include <iostream>
 #include <string>
 #include <string_view>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
+
+#include <absl/container/flat_hash_map.h>
 
 #include "roq/format.h"
 #include "roq/server.h"
@@ -56,7 +56,7 @@ class Config final
  public:
   std::vector<User> users;
   server::Symbols symbols;
-  std::unordered_map<std::string, Account> accounts;
+  absl::flat_hash_map<std::string, Account> accounts;
 };
 
 }  // namespace kraken
