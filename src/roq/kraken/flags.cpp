0c88f790de0578502103a24b8eb64fe67305c55d
thraneh <thraneh@gmail.com>
2021-02-07 06:29:38 +0100

    validation of flags

--- src/roq/kraken/flags.cpp
+++ src/roq/kraken/flags.cpp
@@ -7,60 +7,132 @@
 
 #include <string>
 
-ABSL_FLAG(std::string, config_file, "", "config file (path)");
+#include "roq/core/flags/non_empty.h"
+#include "roq/core/flags/non_zero.h"
+#include "roq/core/flags/path.h"
+#include "roq/core/flags/uri.h"
 
-ABSL_FLAG(std::string, exchange, "kraken", "exchange identifier (string)");
+ABSL_FLAG(  //
+    roq::core::flags::Path<std::string>,
+    config_file,
+    {},
+    "config file (path)");
+
+ABSL_FLAG(  //
+    roq::core::flags::NonEmpty<std::string>,
+    exchange,
+    {"kraken"},
+    "exchange identifier (string)");
 
 // rest
 
-ABSL_FLAG(std::string, rest_uri, "https://api.kraken.com", "REST end-point (URI)");
+ABSL_FLAG(  //
+    roq::core::flags::URI<std::string>,
+    rest_uri,
+    {"https://api.kraken.com"},
+    "REST end-point (URI)");
 
-ABSL_FLAG(uint32_t, rest_ping_freq_secs, 5, "ping frequency (seconds)");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    rest_ping_freq_secs,
+    uint32_t{5},
+    "ping frequency (seconds)");
 
-ABSL_FLAG(
-    std::string,
+ABSL_FLAG(  //
+    roq::core::flags::Path<std::string>,
     rest_ping_path,
-    "/0/public/Time",
+    {"/0/public/Time"},
     "URI path used for REST connection keep-alive messages");
 
-ABSL_FLAG(uint32_t, rest_request_queue_depth, 5, "request: max queue depth");
+ABSL_FLAG(  //
+    uint32_t,
+    rest_request_queue_depth,
+    uint32_t{5},
+    "request: max queue depth");
 
-ABSL_FLAG(uint32_t, rest_request_timeout_secs, 30, "request: timeout (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    rest_request_timeout_secs,
+    uint32_t{30},
+    "request: timeout (seconds)");
 
-ABSL_FLAG(uint32_t, rest_rate_limit_interval_secs, 1, "rate limit: monitor interval (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    rest_rate_limit_interval_secs,
+    uint32_t{1},
+    "rate limit: monitor interval (seconds)");
 
-ABSL_FLAG(uint32_t, rest_rate_limit_max_requests, 300, "rate limit: max requests (per interval)");
+ABSL_FLAG(  //
+    uint32_t,
+    rest_rate_limit_max_requests,
+    uint32_t{300},
+    "rate limit: max requests (per interval)");
 
 // ws public
 
-ABSL_FLAG(std::string, ws_public_uri, "wss://beta-ws.kraken.com", "WebSocket end-point (URI)");
+ABSL_FLAG(  //
+    roq::core::flags::URI<std::string>,
+    ws_public_uri,
+    {"wss://beta-ws.kraken.com"},
+    "WebSocket end-point (URI)");
 
-ABSL_FLAG(uint32_t, ws_public_ping_freq_secs, 5, "ping frequency (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_public_ping_freq_secs,
+    uint32_t{5},
+    "ping frequency (seconds)");
 
-ABSL_FLAG(uint32_t, ws_public_request_timeout_secs, 15, "request time-out (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_public_request_timeout_secs,
+    uint32_t{15},
+    "request time-out (seconds)");
 
-ABSL_FLAG(uint32_t, ws_public_subscribe_book_depth, 10, "book depth");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_public_subscribe_book_depth,
+    uint32_t{10},
+    "book depth");
 
 // ws private
 
-ABSL_FLAG(
-    std::string, ws_private_uri, "wss://beta-ws-auth.kraken.com", "WebSocket end-point (URI)");
+ABSL_FLAG(  //
+    roq::core::flags::URI<std::string>,
+    ws_private_uri,
+    {"wss://beta-ws-auth.kraken.com"},
+    "WebSocket end-point (URI)");
 
-ABSL_FLAG(uint32_t, ws_private_ping_freq_secs, 5, "ping frequency (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_private_ping_freq_secs,
+    uint32_t{5},
+    "ping frequency (seconds)");
 
-ABSL_FLAG(uint32_t, ws_private_request_timeout_secs, 15, "request time-out (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_private_request_timeout_secs,
+    uint32_t{15},
+    "request time-out (seconds)");
 
 // XXX review
 
-ABSL_FLAG(uint32_t, encode_buffer_size, 1048576, "encode buffer size");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    encode_buffer_size,
+    uint32_t{1048576},
+    "encode buffer size");
 
-ABSL_FLAG(uint32_t, decode_buffer_size, 10485760, "decode buffer size");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    decode_buffer_size,
+    uint32_t{10485760},
+    "decode buffer size");
 
 // external
 
-ABSL_DECLARE_FLAG(std::string, name);
-ABSL_DECLARE_FLAG(uint32_t, cache_mbp_max_depth);
-ABSL_DECLARE_FLAG(uint32_t, cache_trades_max_depth);
+ABSL_DECLARE_FLAG(roq::core::flags::NonEmpty<std::string>, name);
+ABSL_DECLARE_FLAG(roq::core::flags::NonZero<uint32_t>, cache_mbp_max_depth);
+ABSL_DECLARE_FLAG(roq::core::flags::NonZero<uint32_t>, cache_trades_max_depth);
 
 namespace roq {
 namespace kraken {
