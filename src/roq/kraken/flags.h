9c207e138eed9315c3a2d1dfeea576842333f5d9
thraneh <thraneh@gmail.com>
2021-03-10 11:52:25 +0100

    ensure all usage of flags (using abseil-cpp) will compile with the default standard version

---
+++ src/roq/kraken/flags.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/kraken/flags/flags.h"
+
+namespace roq {
+namespace kraken {
+
+using flags::Flags;
+
+}  // namespace kraken
+}  // namespace roq
