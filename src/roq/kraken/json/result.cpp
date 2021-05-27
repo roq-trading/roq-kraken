98053e0c8a24aa8ffd429227770d3b4b8fd64b27
thraneh <thraneh@gmail.com>
2021-05-27 10:18:20 +0200

    align OrderStatus and TimeInForce with FIX

--- src/roq/kraken/json/result.cpp
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2017-2021, Hans Erik Thrane */
-
-#include "roq/kraken/json/result.h"
-
-namespace roq {
-namespace kraken {
-namespace json {}  // namespace json
-}  // namespace kraken
-}  // namespace roq
