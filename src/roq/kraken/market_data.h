e8628df9838de2bca448b679e6c10eb5c76302ed
thraneh <thraneh@gmail.com>
2021-11-15 17:42:22 +0100

    Support server-side web-socket + rename client-side web-socket

--- src/roq/kraken/market_data.h
+++ src/roq/kraken/market_data.h
@@ -12,7 +12,7 @@
 
 #include "roq/core/io/context.h"
 
-#include "roq/core/web/socket.h"
+#include "roq/core/web/client_socket.h"
 
 #include "roq/download.h"
 #include "roq/server.h"
@@ -25,7 +25,7 @@
 namespace roq {
 namespace kraken {
 
-class MarketData final : public core::web::Socket::Handler, public json::ParserPublic::Handler {
+class MarketData final : public core::web::ClientSocket::Handler, public json::ParserPublic::Handler {
  public:
   struct Handler {
     virtual void operator()(const server::Trace<StreamStatus> &) = 0;
@@ -50,13 +50,13 @@ class MarketData final : public core::web::Socket::Handler, public json::ParserP
   void update_subscriptions(std::vector<std::string> &symbols);
 
  protected:
-  void operator()(const core::web::Socket::Connected &) override;
-  void operator()(const core::web::Socket::Disconnected &) override;
-  void operator()(const core::web::Socket::Ready &) override;
-  void operator()(const core::web::Socket::Close &) override;
-  void operator()(const core::web::Socket::Latency &) override;
-  void operator()(const core::web::Socket::Text &) override;
-  void operator()(const core::web::Socket::Binary &) override;
+  void operator()(const core::web::ClientSocket::Connected &) override;
+  void operator()(const core::web::ClientSocket::Disconnected &) override;
+  void operator()(const core::web::ClientSocket::Ready &) override;
+  void operator()(const core::web::ClientSocket::Close &) override;
+  void operator()(const core::web::ClientSocket::Latency &) override;
+  void operator()(const core::web::ClientSocket::Text &) override;
+  void operator()(const core::web::ClientSocket::Binary &) override;
 
   void operator()(ConnectionStatus);
 
@@ -94,7 +94,7 @@ class MarketData final : public core::web::Socket::Handler, public json::ParserP
   const uint16_t stream_id_;
   const std::string name_;
   // web socket
-  core::web::Socket connection_;
+  core::web::ClientSocket connection_;
   // buffers
   core::Buffer decode_buffer_;
   // metrics
