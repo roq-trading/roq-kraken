06058bd1311bd7a34286e009c5f1760bf6d6fff3
thraneh <thraneh@gmail.com>
2022-02-17 08:42:27 +0100

    update submodules + auto-format code

--- src/roq/kraken/drop_copy.h
+++ src/roq/kraken/drop_copy.h
@@ -26,7 +26,8 @@
 namespace roq {
 namespace kraken {
 
-class DropCopy final : public core::web::ClientSocket::Handler, public json::ParserPrivate::Handler {
+class DropCopy final : public core::web::ClientSocket::Handler,
+                       public json::ParserPrivate::Handler {
  public:
   struct Handler {
     virtual void operator()(const server::Trace<StreamStatus> &) = 0;
