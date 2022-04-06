9643b74e3ace706606fdb8e6dbf3c5a49584cc52
thraneh <thraneh@gmail.com>
2022-04-06 15:33:48 +0200

    use std::strong_ordering for comparisons + chore

--- src/roq/kraken/gateway.cpp
+++ src/roq/kraken/gateway.cpp
@@ -131,7 +131,7 @@ uint16_t Gateway::operator()(
     const std::string_view &request_id,
     const std::string_view &previous_request_id) {
   assert(!std::empty(event.value.account));
-  assert(utils::compare(event.value.account, order.account) == 0);
+  assert(event.value.account == order.account);
   return get_order_entry(event.value.account)(event, order, request_id, previous_request_id);
 }
 
@@ -141,7 +141,7 @@ uint16_t Gateway::operator()(
     const std::string_view &request_id,
     const std::string_view &previous_request_id) {
   assert(!std::empty(event.value.account));
-  assert(utils::compare(event.value.account, order.account) == 0);
+  assert(event.value.account == order.account);
   return get_order_entry(event.value.account)(event, order, request_id, previous_request_id);
 }
 
