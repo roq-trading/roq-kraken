225c7a6559ec8238721d5899524cec81a7b2db7a
thraneh <thraneh@gmail.com>
2022-07-16 12:59:29 +0200

    clean-up roq-io directory structure

--- src/roq/kraken/gateway.cpp
+++ src/roq/kraken/gateway.cpp
@@ -4,7 +4,7 @@
 
 #include <utility>
 
-#include "roq/io/event/context_factory.hpp"
+#include "roq/io/engine/libevent/context_factory.hpp"
 
 #include "roq/kraken/flags.hpp"
 
@@ -41,7 +41,7 @@ auto create_drop_copy(T &security) {
 
 Gateway::Gateway(server::Dispatcher &dispatcher, Config const &config)
     : dispatcher_(dispatcher), master_account_(config.get_master_account()),
-      security_(create_security<decltype(security_)>(config)), context_(io::event::ContextFactory::create()),
+      security_(create_security<decltype(security_)>(config)), context_(io::engine::libevent::ContextFactory::create()),
       shared_(dispatcher), rest_(*this, *context_, ++stream_id_, shared_),
       order_entry_(create_order_entry<decltype(order_entry_)>(*this, *context_, stream_id_, security_, shared_)),
       drop_copy_(create_drop_copy<decltype(drop_copy_)>(security_)) {
