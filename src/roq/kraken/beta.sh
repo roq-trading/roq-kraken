1237c1b91bb9f9f6bdd95b7d1b6aee2779858b31
thraneh <thraneh@gmail.com>
2022-10-29 04:44:42 +0200

    rename ParametersUpdate (from ParameterUpdate)

--- src/roq/kraken/beta.sh
+++ src/roq/kraken/beta.sh
@@ -28,7 +28,7 @@ $PREFIX ./roq-kraken \
   --event_log_dir "$HOME/var/lib/roq/data" \
   --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
-  --metrics_listen_address "$HOME/run/metrics/${NAME}.sock" \
+  --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
   --rest_uri "$REST_URI" \
   --ws_public_uri "$WS_PUBLIC_URI" \
   --ws_private_uri "$WS_PRIVATE_URI" \
