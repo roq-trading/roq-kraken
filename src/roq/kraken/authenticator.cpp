3108e646e95842333b9f60dac5d0fbb108a7ec04
thraneh <thraneh@gmail.com>
2023-04-03 20:25:21 +0200

    formatting

--- src/roq/kraken/authenticator.cpp
+++ src/roq/kraken/authenticator.cpp
@@ -8,10 +8,8 @@ namespace kraken {
 // === IMPLEMENTATION ===
 
 Authenticator::Authenticator(Config const &config, std::string_view const &account)
-    : account_{account}, crypto_{
-                             config.get_access_key(account),
-                             config.get_access_secret(account),
-                             config.get_access_password(account)} {
+    : account_{account},
+      crypto_{config.get_access_key(account), config.get_access_secret(account), config.get_access_password(account)} {
 }
 
 std::string Authenticator::create_body() {
