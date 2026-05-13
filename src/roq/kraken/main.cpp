c27a9bc9887bf5fcd75e92ceb999e179a2579532
thraneh <thraneh@gmail.com>
2026-05-13 14:18:44 +0000

    chore

--- src/roq/kraken/main.cpp
+++ src/roq/kraken/main.cpp
@@ -11,7 +11,7 @@ using namespace std::literals;
 
 namespace {
 auto const INFO = roq::Service::Info{
-    .description = "Roq Kraken Gateway"sv,
+    .description = "Roq's Kraken gateway"sv,
     .package_name = ROQ_PACKAGE_NAME,
     .host = ROQ_HOST,
     .build_version = ROQ_BUILD_VERSION,
