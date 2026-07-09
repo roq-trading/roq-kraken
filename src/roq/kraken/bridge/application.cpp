9f55dd9745b67d716d919aa1b5bff06365ea54a0
thraneh <thraneh@gmail.com>
2026-07-09 15:09:49 +0000

    rename *-bridge (from *-proto-bridge)

--- src/roq/kraken/bridge/application.cpp
+++ src/roq/kraken/bridge/application.cpp
@@ -1,21 +1,21 @@
 /* Copyright (c) 2017-2026, Hans Erik Thrane */
 
-#include "roq/kraken/proto_bridge/application.hpp"
+#include "roq/kraken/bridge/application.hpp"
 
 #include "roq/logging.hpp"
 
-#include "roq/server/proto_bridge/controller.hpp"
+#include "roq/server/bridge/controller.hpp"
 
 #include "roq/kraken/gateway/controller.hpp"
 
-#include "roq/kraken/proto_bridge/config.hpp"
-#include "roq/kraken/proto_bridge/settings.hpp"
+#include "roq/kraken/bridge/config.hpp"
+#include "roq/kraken/bridge/settings.hpp"
 
 using namespace std::literals;
 
 namespace roq {
 namespace kraken {
-namespace proto_bridge {
+namespace bridge {
 
 // === IMPLEMENTATION ===
 
@@ -24,10 +24,10 @@ int Application::main(args::Parser const &args) {
   Config config{settings};
   log::warn("config={}"sv, config);
   auto context = server::create_io_context(settings);
-  server::proto_bridge::Controller<gateway::Controller>{settings, config, *context}.dispatch();
+  server::bridge::Controller<gateway::Controller>{settings, config, *context}.dispatch();
   return EXIT_SUCCESS;
 }
 
-}  // namespace proto_bridge
+}  // namespace bridge
 }  // namespace kraken
 }  // namespace roq
--- src/roq/kraken/bridge/config.hpp
+++ src/roq/kraken/bridge/config.hpp
@@ -6,10 +6,10 @@
 
 namespace roq {
 namespace kraken {
-namespace proto_bridge {
+namespace bridge {
 
 using Config = gateway::Config;
 
-}  // namespace proto_bridge
+}  // namespace bridge
 }  // namespace kraken
 }  // namespace roq
